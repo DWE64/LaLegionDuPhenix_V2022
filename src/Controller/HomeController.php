cdfad0afffcb887d3d81e9081a0ba69d8160194e
DWE64 <developpement.web.entreprise64@gmail.com>
2022-06-25 11:31:58 +0200

    installation du thème Hyper pour l'admin, le bureau et le profil user

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class HomeController extends AbstractController
+{
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'title' => $this->translator->trans('page.home'),
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/change_locale/{locale}', name:'change_locale')]
+    public function changeLocale($locale, Request $request)
+    {
+        $request->getSession()->set('_locale',$locale);
+
+        return $this->redirect($request->headers->get('referer'));
+    }
+}
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,12 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
     {
+        $this->translator = $translator;
+    }
+
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(
+        AuthenticationUtils $authenticationUtils
+    ): Response {
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
         // }
@@ -21,12 +30,20 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig',
+                             [
+                                 'title' => $this->translator->trans('page.login'),
+                                 'last_username' => $lastUsername,
+                                 'error' => $error
+                             ]
+        );
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new \LogicException(
+            'This method can be blank - it will be intercepted by the logout key on your firewall.'
+        );
     }
 }
